2d9c954eedff182e1e467c829131c1b2608f3c25
Sergei-V-Fedorov <106704782+Sergei-V-Fedorov@users.noreply.github.com>
2022-06-02 00:49:45 +0300

    add task 25-03

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+class MyDict(dict):
+
+    def get(self, key, fill=0):
+        if key in self.keys():
+            return self[key]
+        else:
+            return fill
+
+
+my_dict = MyDict()
+my_dict[1] = 10
+
+value = my_dict.get(1)
+print(f"{1}: {value}")
+
+value = my_dict.get(5)
+print(f"{5}: {value}")
