03ccc62f5eb70a492747511539136a703e60fbe3
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2022-06-02 23:59:16 +0300

    add annotation in taxes.py

---
+++ Module25/01_taxes/taxes.py
@@ -0,0 +1,32 @@
+class Property:
+    """ Клаcc для описания имущества """
+    def __init__(self, worth: int = 1000) -> None:
+        self.worth = worth
+
+    def calculate_tax(self, tax: int = 10) -> float:
+        return self.worth * tax / 100
+
+
+class Apartment(Property):
+    def __init__(self, worth: int) -> None:
+        super().__init__(worth)
+
+    def calculate_tax(self, tax: float = 0.1) -> float:
+        return self.worth * tax / 100
+
+
+class Car(Property):
+    def __init__(self, worth: int) -> None:
+        super().__init__(worth)
+
+    def calculate_tax(self, tax: float = 0.5) -> float:
+        return self.worth * tax / 100
+
+
+class CountryHouse(Property):
+    """Класс расчета налога на загородный дом"""
+    def __init__(self, worth):
+        super().__init__(worth)
+
+    def calculate_tax(self, tax: float = 0.2) -> float:
+        return self.worth * tax / 100
